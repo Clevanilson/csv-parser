d5df9b7028811d38591c999c59eae6153947d7c8
clevanilson <clevanilson.silvaoliveira@gmail.com>
2022-11-26 14:02:51 -0300

    feat(parser): add function parse_row and type TableRow

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,42 @@
+pub mod types;
+
+use std::collections::HashMap;
+
+pub use types::*;
+
+
+pub fn parse_row(line: &str, headers: &Vec<String>) -> TableRow {
+    let mut row: TableRow = HashMap::new();
+    let mut it_is_in_quotes = false;
+    let mut initial_index = 0;
+    let mut header_index = 0;
+
+    for (index, char) in line.chars().enumerate() {
+        match char {
+            '"' => it_is_in_quotes = !it_is_in_quotes,
+            ',' => {
+                if !it_is_in_quotes {
+                    let value = &line[initial_index..index];
+                    let value = remove_comma(value);
+                    let header = headers.get(header_index).expect("Header does not exist");
+                    row.insert(header.clone(), value);
+                    initial_index = index + 1;
+                    header_index = header_index + 1;
+                }
+            },
+            _ => {},
+        }
+    }
+
+    let value = &line[initial_index..];
+    let value = remove_comma(value);
+    let header = headers.get(header_index).expect("Header does not exist");
+    row.insert(header.clone(), value);
+
+
+    return row;
+}
+
 pub fn parse_line(line: &str) -> Vec<String> {
     let mut tokens = Vec::new();
     let mut it_is_in_quotes = false;
---
+++ src/core/types.rs
@@ -0,0 +1,3 @@
+use std::collections::HashMap;
+
+pub type TableRow = HashMap<String, String>;
